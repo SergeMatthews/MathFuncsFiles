e08d9527a65be9701e3911cca746e17d86bcb918
SergeMatthews <sergei_matveichuk94@mail.ru>
2025-11-16 21:32:51 +0600

    Update MyMathFuncs.cpp

--- MathFuncs/MyMathFuncs.cpp
+++ MathFuncs/MyMathFuncs.cpp
@@ -11,17 +11,17 @@ void MySubtr(double a, double b) {
 	cout << a << " - " << b << " = " << (a - b) << endl;
 }
 void MyMult(double a, double b) {
-	cout << a << " óěíîćčňü íŕ " << b << " = " << (a * b) << endl;
+	cout << a << " умножить на " << b << " = " << (a * b) << endl;
 }
 void MyDiv(double a, double b) {
 	if (b)
-		cout << a << " đŕçäĺëčňü íŕ " << b << " = " << (a / b) << endl;
+		cout << a << " разделить на " << b << " = " << (a / b) << endl;
 	else
-		cout << a << " đŕçäĺëčňü íŕ " << b << " = " << 0 << endl;
+		cout << a << " разделить на " << b << " = " << 0 << endl;
 }
 void MyPow(double a, double b) {
 	if (a && b)
-		cout << a << " â ńňĺďĺíč " << b << " = " << pow(a, b) << endl;
+		cout << a << " в степени " << b << " = " << pow(a, b) << endl;
 	else
-		cout << a << " â ńňĺďĺíč " << b << " = " << 1 << endl;
+		cout << a << " в степени " << b << " = " << 1 << endl;
 }
